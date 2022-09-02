2b6d61b9e9f902628a97ca4f57ee1c39f795926e
Artsiom <sorintema@gmail.com>
2022-09-02 18:01:02 +0300

    INIT

---
+++ services/log.service.js
@@ -0,0 +1,22 @@
+import chalk from "chalk";
+import dedent from "dedent-js";
+
+const printError = (error) => {
+    console.log(chalk.bgRed("ERROR") + " " + error);
+};
+
+const printSuccess = (message) => {
+    console.log(chalk.bgGreen("SUCCESS" + " " + message));
+};
+
+const printHelper = () => {
+    console.log(dedent(`
+    ${chalk.bgCyan("HELP")}
+    без параметров - вывод погоды
+    -s [CITY] для установки города
+    -h помощь
+    -t сохранение токена
+    `));
+};
+
+export { printSuccess, printError, printHelper };
